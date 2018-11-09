bfe6a991bb531a507b74fe4847ccdb5e52914afb
paulorgn <paulorgndev@gmail.com>
2018-11-09 18:23:45 -0200

    Componente adicionado

--- Projeto/src/app/app.module.ts
+++ Projeto/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NovoComponenteComponent } from './novo-componente/novo-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovoComponenteComponent
   ],
   imports: [
     BrowserModule,
